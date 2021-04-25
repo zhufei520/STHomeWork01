f3193f2ca8b1ca71af83e8f87177ea3c0167abd4
zhufei520 <1625776436@qq.com>
2021-04-25 21:54:33 +0800

    Add files via upload

---
+++ StudentManager.java
@@ -0,0 +1,41 @@
+package Student;
+public class StudentManager {
+    private String stuNum;//学号
+    private String name;//姓名
+    private String gender;//性别
+    private int age;//年龄
+    private int score;//分数
+    public StudentManager() {
+        super();
+    }
+    public String getStuNum() {
+        return stuNum;
+    }
+    public void setStuNum(String stuNum) {
+        this.stuNum = stuNum;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }   
+}
